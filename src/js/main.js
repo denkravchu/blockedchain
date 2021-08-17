e75a1ab6f6381509e9b81f9429b3c1c32bf1b08e
Denis Kravchu <denkravchu@gmail.com>
2021-08-17 09:54:57 +0200

    added new animation

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,38 @@ document.getElementsByClassName("mobile-nav")[0].addEventListener("click", funct
 document.getElementsByClassName("mobile-close-nav")[0].addEventListener("click", function() {
 	toggleMenu();
 });
+
+// Ring skew ---------------------------
+window.addEventListener('load', () => {
+	skewRing()
+})
+
+function skewRing() {
+	const ring = document.querySelector('#ring')
+	if (!ring) return
+
+	window.addEventListener('mousemove', e => {
+		const cursorPosition = getCursorPositionFromCenter(e)
+		const skewMatrix = getSkewMatrix(cursorPosition)
+		ring.style.cssText = `
+			transform: matrix(${skewMatrix});
+			transition: transform .1s ease;
+		`
+	})
+}
+
+function getCursorPositionFromCenter(e) {
+	return {
+		x: e.clientX - window.innerWidth / 2,
+		y: e.clientY - window.innerHeight / 2
+	}
+}
+
+function getSkewMatrix(cursorPosition) {
+	const m = {
+		a: 1 - cursorPosition.x / 5000, b: cursorPosition.x / 5000,
+		c: cursorPosition.y / 5000, d: 1,
+		tx: cursorPosition.x / 10, ty: cursorPosition.y / 10
+	}
+	return `${m.a}, ${m.b}, ${m.c}, ${m.d}, ${m.tx}, ${m.ty}`
+}
